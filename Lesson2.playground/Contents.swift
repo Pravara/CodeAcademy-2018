75d30fa0e34c63bf9445a8c405a39d07b943e76e
Pravara Kant <pkant@hotels.com>
2018-02-06 15:10:55 +0000

    Introduction to Swift playground

---
+++ Lesson2.playground/Contents.swift
@@ -0,0 +1,70 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//: Functions
+//func greet() {
+//    print("Hello World!")
+//}
+
+//greet()
+
+//func greet(name: String) {
+//    print("Hello \(name)")
+//}
+
+//greet(name: "iOS")
+
+//func greet(withName name: String) {
+//    print("Hi \(name)")
+//}
+
+//greet(withName: "Swift")
+
+//: if - else
+//enum timeOfDay {
+//    case morning
+//    case afternoon
+//    case evening
+//    case night
+//}
+
+//func greet(name: String, time: timeOfDay) {
+//    if time == .morning {
+//        print("Good Morning \(name)")
+//    } else if (time == .night){
+//        print("Good Night \(name)")
+//    } else {
+//        print("Good Day \(name)")
+//    }
+//
+//    // switch statement
+//    var greeting: String
+//    switch time {
+//    case .morning:
+//        greeting = "Morning"
+//    case .afternoon:
+//        greeting = "Afternoon"
+//    case .evening:
+//        greeting = "Evening"
+//    case .night:
+//        greeting = "Night"
+//    }
+//    print("Good \(greeting) \(name)")
+//}
+
+//greet(name: "London", time: .afternoon)
+
+//: For Loop
+//func greet(_ name: String, numberOfTimes: Int) {
+//    for i  in 1...numberOfTimes {
+//        print("hey \(name) \(i)")
+//    }
+//}
+
+//greet("friend", numberOfTimes: 3)
+
+//: Optionals
+
